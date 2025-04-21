563463ea71a157b5b098bf5a9274677bd1bb4d3f
Carolina Silva <117127789+crlnslvr@users.noreply.github.com>
2025-04-21 14:07:50 -0300

    Update Main.java
    chore: criar classe Main para testar manualmente os cálculos no terminal

--- Main.java
+++ Main.java
@@ -1,2 +1,16 @@
+import java.time.LocalDateTime;
+
 public class Main {
+    public static void main(String[] args) {
+        InfosEstacionamento infos = new InfosEstacionamento();
+        CalculadoraEstacionamento calc = new CalculadoraEstacionamento(infos);
+
+        //Teste manual
+        LocalDateTime entrada = LocalDateTime.of(2025, 4, 18, 8, 0);
+        LocalDateTime saida = LocalDateTime.of(2025, 4, 18, 8, 21);
+        boolean vip = false; 
+
+        double valor = calc.calcularTarifa(entrada, saida, vip);
+        System.out.println("Valor calculado: R$" + valor);
+    }
 }
